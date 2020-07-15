a343bbad786d47ceb210df9d82bba6f2b5b7a356
razor2701 <dabre.necil@gmail.com>
2020-07-15 22:13:06 +0530

    Initial Commit

---
+++ src/main/java/com/nd/restmongoDB/RestMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.nd.restmongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nd/restmongoDB/RestMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nd.restmongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
